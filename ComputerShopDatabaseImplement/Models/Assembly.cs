af58989bfb9be30181ffbdd1d0f51fd3195e46b2
Alexander-Goryaynov <gor.alex595@gmail.com>
2020-03-19 14:55:35 +0400

    lab3done

---
+++ ComputerShopDatabaseImplement/Models/Assembly.cs
@@ -0,0 +1,22 @@
+﻿using ComputerShopDatabaseImplement.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace ComputerShopDatabaseImplement.Models
+{
+    public class Assembly
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [ForeignKey("AssemblyId")]
+        public virtual List<AssemblyDetail> AssemblyDetails { get; set; }
+        [ForeignKey("OrderId")]
+        public virtual List<Order> Order { get; set; }
+    }
+}
