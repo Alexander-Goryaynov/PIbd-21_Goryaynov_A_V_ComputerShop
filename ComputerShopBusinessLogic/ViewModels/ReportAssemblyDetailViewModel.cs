b9c77e37fe37c1a6e4c6492ad8a3eaa5213206f5
Alexander-Goryaynov <gor.alex595@gmail.com>
2020-03-20 14:35:27 +0400

    lab4 transitional

---
+++ ComputerShopBusinessLogic/ViewModels/ReportAssemblyDetailViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputerShopBusinessLogic.ViewModels
+{
+    public class ReportAssemblyDetailViewModel
+    {        
+        public string DetailName { get; set; }
+        public int TotalCount { get; set; }
+        public List<Tuple<string, int>> Assemblies { get; set; }        
+    }
+}
