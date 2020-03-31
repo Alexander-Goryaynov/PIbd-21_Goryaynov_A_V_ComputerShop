cbf30b3bba7f62b4b0bc6f95421f6d5fd401ce14
Alexander-Goryaynov <gor.alex595@gmail.com>
2020-03-31 21:21:39 +0400

    lab4transitional2

---
+++ ComputerShopBusinessLogic/HelperModels/ExcelInfo.cs
@@ -0,0 +1,14 @@
+﻿using ComputerShopBusinessLogic.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputerShopBusinessLogic.HelperModels
+{
+    class ExcelInfo
+    {
+        public string FileName { get; set; }
+        public string Title { get; set; }
+        public List<ReportAssemblyDetailViewModel> AssemblyDetails { get; set; }
+    }
+}
